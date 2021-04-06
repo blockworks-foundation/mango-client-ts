0216c58da8f298bba32b56ac90e2286af7d75780
dd <dafydd.durairaj@gmail.com>
2021-04-06 16:43:00 -0400

    Added make functions in instructions.ts

--- src/instruction.ts
+++ src/instruction.ts
@@ -5,7 +5,7 @@ import {
   TransactionInstruction,
 } from '@solana/web3.js';
 import { Order } from '@project-serum/serum/lib/market';
-import { encodeMangoInstruction } from './layout';
+import { encodeMangoInstruction, NUM_TOKENS } from './layout';
 import { TOKEN_PROGRAM_ID } from '@project-serum/serum/lib/token-instructions';
 import { uiToNative } from './utils';
 
@@ -133,4 +133,81 @@ export function makeSettleBorrowInstruction(
     SettleBorrow: { tokenIndex: new BN(tokenIndex), quantity: nativeQuantity },
   });
   return new TransactionInstruction({ keys, data, programId });
+}
+
+export function makeForceCancelOrdersInstruction(
+  programId: PublicKey,
+  mangoGroup: PublicKey,
+  liqor: PublicKey,
+  liqeeMarginAccount: PublicKey,
+  baseVault: PublicKey,
+  quoteVault: PublicKey,
+  spotMarket: PublicKey,
+  bids: PublicKey,
+  asks: PublicKey,
+  signerKey: PublicKey,
+  dexEventQueue: PublicKey,
+  dexBaseVault: PublicKey,
+  dexQuoteVault: PublicKey,
+  dexSigner: PublicKey,
+  dexProgramId: PublicKey,
+  openOrders: PublicKey[],
+  oracles: PublicKey[]
+): TransactionInstruction {
+
+  const keys = [
+    { isSigner: false, isWritable: true, pubkey: mangoGroup},
+    { isSigner: true, isWritable: false, pubkey: liqor },
+    { isSigner: false,  isWritable: true, pubkey: liqeeMarginAccount },
+    { isSigner: false,  isWritable: true, pubkey: baseVault },
+    { isSigner: false,  isWritable: true, pubkey: quoteVault },
+    { isSigner: false,  isWritable: true, pubkey: spotMarket },
+    { isSigner: false,  isWritable: true, pubkey: bids },
+    { isSigner: false,  isWritable: true, pubkey: asks },
+    { isSigner: false,  isWritable: false, pubkey: signerKey },
+    { isSigner: false,  isWritable: true, pubkey: dexEventQueue },
+    { isSigner: false, isWritable: true, pubkey: dexBaseVault },
+    { isSigner: false, isWritable: true, pubkey: dexQuoteVault },
+    { isSigner: false, isWritable: false, pubkey: dexSigner },
+    { isSigner: false, isWritable: false, pubkey: TOKEN_PROGRAM_ID },
+    { isSigner: false, isWritable: false, pubkey: dexProgramId },
+    { isSigner: false, isWritable: false, pubkey: SYSVAR_CLOCK_PUBKEY },
+    ...openOrders.map( (pubkey) => ( { isSigner: false, isWritable: true, pubkey })),
+    ...oracles.map( (pubkey) => ( { isSigner: false, isWritable: false, pubkey })),
+  ]
+
+  const data = encodeMangoInstruction({ForceCancelOrders: {}})
+  return new TransactionInstruction( { keys, data, programId })
+}
+
+export function makePartialLiquidateInstruction(
+  programId: PublicKey,
+  mangoGroup: PublicKey,
+  liqor: PublicKey,
+  liqorInTokenWallet: PublicKey,
+  liqorOutTokenWallet: PublicKey,
+  liqeeMarginAccount: PublicKey,
+  inTokenVault: PublicKey,
+  outTokenVault: PublicKey,
+  signerKey: PublicKey,
+  openOrders: PublicKey[],
+  oracles: PublicKey[],
+  maxDeposit: BN
+): TransactionInstruction {
+  const keys = [
+    { isSigner: false, isWritable: true, pubkey: mangoGroup },
+    { isSigner: true, isWritable: false, pubkey: liqor },
+    { isSigner: false, isWritable: true, pubkey: liqorInTokenWallet },
+    { isSigner: false, isWritable: true, pubkey: liqorOutTokenWallet },
+    { isSigner: false,  isWritable: true, pubkey: liqeeMarginAccount },
+    { isSigner: false, isWritable: true, pubkey: inTokenVault },
+    { isSigner: false, isWritable: true, pubkey: outTokenVault },
+    { isSigner: false, isWritable: false, pubkey: signerKey },
+    { isSigner: false, isWritable: false, pubkey: SYSVAR_CLOCK_PUBKEY },
+    ...openOrders.map( (pubkey) => ( { isSigner: false, isWritable: false, pubkey })),
+    ...oracles.map( (pubkey) => ( { isSigner: false, isWritable: false, pubkey })),
+  ]
+  const data = encodeMangoInstruction({PartialLiquidate: { maxDeposit }})
+
+  return new TransactionInstruction( { keys, data, programId })
 }
