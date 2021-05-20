0f47d83101815b66bfc301df390c1d19efe2da36
Ralfs <lagzda7@inbox.lv>
2021-05-20 21:44:11 +0300

    Updated ids and layout

--- src/layout.ts
+++ src/layout.ts
@@ -2,7 +2,7 @@ import { bits, BitStructure, Blob, Layout, seq, struct, u32, u8, u16, UInt, unio
 import { PublicKey } from '@solana/web3.js';
 import BN from 'bn.js';
 
-export const NUM_TOKENS = 3;
+export const NUM_TOKENS = 5;
 export const NUM_MARKETS = NUM_TOKENS - 1;
 export const MANGO_GROUP_PADDING = 8 - (NUM_TOKENS + NUM_MARKETS) % 8;
 export const MAX_RATE = 3.0
@@ -284,5 +284,3 @@ export function encodeMangoInstruction(data) {
   const span = MangoInstructionLayout.encode(data, b);
   return b.slice(0, span);
 }
-
-
