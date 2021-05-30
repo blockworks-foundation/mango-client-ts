e619b7aad3c965583c6b7076379bfff3100ff24f
feierabend654 <feierabend654@gmail.com>
2021-05-30 12:50:39 +0200

    export simple client
    Signed-off-by: feierabend654 <feierabend654@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import IDS from './ids.json';
 export { IDS }
 export { MangoClient, MangoGroup, MarginAccount, tokenToDecimals } from './client';
+export { SimpleClient } from './simpleclient';
 export { MangoIndexLayout, MarginAccountLayout, MangoGroupLayout } from './layout';
 export * from './layout';
 export * from './utils';
